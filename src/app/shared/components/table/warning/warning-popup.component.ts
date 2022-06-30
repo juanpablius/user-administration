a5a03e566e55c59d7ea4f7793a79e45f8050640e
Juan Pablo Castro <juanpablius@gmail.com>
2022-07-01 01:39:16 +0200

    warning component

---
+++ src/app/shared/components/table/warning/warning-popup.component.ts
@@ -0,0 +1,50 @@
+import {Component, Inject, Optional} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {Action} from 'src/app/core/interfaces/pop-up';
+
+export interface WarningPopupInput {
+  errorMessage?: string;
+  conflictLines?: any[];
+  headerMessage: string;
+  continue?: boolean;
+}
+
+export interface WarningPopupOutput {
+  event: string;
+}
+
+@Component({
+  templateUrl: './warning-popup.component.html',
+  styleUrls: ['./warning-popup.component.scss'],
+})
+export class WarningPopupComponent {
+  public cancelled = false;
+
+  public constructor(
+      public dialogRef: MatDialogRef<WarningPopupComponent>,
+      @Optional() @Inject(MAT_DIALOG_DATA) public data: WarningPopupInput,
+  ) {
+    dialogRef.disableClose = true;
+  }
+
+  public getItemName(subItem: any): string {
+    return subItem?.key || ''
+  }
+
+  public isAString(val: any): boolean {
+    return typeof val === 'string';
+  }
+
+  public isAnObject(val: any): boolean {
+    return val instanceof (Object);
+  }
+
+  public closeDialog(): void {
+    this.cancelled = true;
+    this.dialogRef.close({event: Action.cancel});
+  }
+
+  public confirm(): void {
+    this.dialogRef.close({event: Action.confirm});
+  }
+}
