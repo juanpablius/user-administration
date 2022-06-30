dc225334882fc51d98cc6a3563aa92d055e06a48
Juan Pablo Castro <juanpablius@gmail.com>
2022-07-01 01:50:48 +0200

    Used oauthConfigService to autologin on the platform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {tap} from 'rxjs';
+
+import {OAuthConfigurationService, UserInfo} from './auth/services/oauth-configuration.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'user-administration';
+export class AppComponent implements OnInit {
+  public title = 'user-administration';
+  public userInfo: UserInfo;
+
+  public constructor(
+      private readonly _oauthConfigurationService: OAuthConfigurationService,
+  ) {
+    this._oauthConfigurationService.loginWithGoogle();
+  }
+
+  public ngOnInit(): void {
+    this._oauthConfigurationService.getUserProfileInfo$()
+        .pipe(
+            tap((userInfo: UserInfo) => this.userInfo = userInfo),
+            )
+        .subscribe();
+  }
+
+  public isLoggedIn(): boolean {
+    return this._oauthConfigurationService.isLoggedIn();
+  }
+
+  public login(): void {
+    this._oauthConfigurationService.loginWithGoogle();
+  };
+
+  public logout(): void {
+    this._oauthConfigurationService.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthModule} from './auth/auth.module';
+import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    // Author
+    AuthModule,
+    CoreModule,
+    SharedModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
